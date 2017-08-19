9436c0372529b9dd9012ee2d3650e5eb9c12cc81
Augusto Branquinho <augustobranquinho@gmail.com>
2017-08-18 21:58:22 -0300

    Projeto do GitHub Pages.

--- src/main/java/org/home/on/utils/ResourceBase.java
+++
@@ -1,8 +0,0 @@
-package org.home.on.utils;
-
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public interface ResourceBase {
-}
--- src/test/resources/data-h2.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `tb_user` (`id`,`name`) VALUES (1,'User 1');
-INSERT INTO `tb_user` (`id`,`name`) VALUES (2,'User 2');
