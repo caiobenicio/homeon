83936d3a1dda52cdf67f729030de1a65ec27856b
Augusto Branquinho <augustobranquinho@gmail.com>
2017-09-15 22:12:48 -0300

    Configurando o liquibase.

--- src/main/java/org/home/on/domain/permission/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.home.on.domain.permission;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<PermissionEntity, Long> {
-
-}
--- src/main/java/org/home/on/domain/userpermission/UserPermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.home.on.domain.userpermission;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPermissionRepository  extends JpaRepository<UserPermissionEntity, UserPermissionKey> {
-
-}
